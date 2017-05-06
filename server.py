80cba78fc76de2b929d37677fec31dce4bd6c78a
vagrant <vagrant@vagrant.vm>
2017-05-06 20:25:03 +0000

    Push up table

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 """Hey Congress!  App"""
 
+from datetime import datetime
 import os
 from jinja2 import StrictUndefined
 from flask import Flask, render_template, redirect, request, flash, session
 # from flask_debugtoolbar import DebugToolbarExtension
 import json
+
 
 
 app = Flask(__name__)
@@ -21,7 +23,132 @@ def index():
     return render_template("homepage.html")
 
 
-if __name__ == "__main__":
+@app.route("/login")
+def login_form():
+    """Show user log in form"""
+
+    if 'user_id' in session:
+        flash('user already signed in')
+        return redirect('/')
+    else:
+        return render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
+def login_process():
+    """Process Log-in, checking password"""
+
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    message = validate_login(username, password)
+    if type(message) is str:
+        flash(message)
+        return redirect("/login")
+    else:
+        user = message
+        flash(("%s Logged In!") % (username))
+        session['user_id'] = user.user_id
+        if user.lat is not None:
+            update_session(user.lat, user.lon)
+
+    return redirect("/users/" + str(user.user_id))
+
+
+@app.route("/register")
+def register_form():
+    """show the registration form"""
+
+    if 'user_id' in session:
+        flash('user already signed in')
+        return redirect('/')
+    else:
+        return render_template("register_form.html")
+
+
+@app.route("/register", methods=["POST"])
+def register_process():
+    """Add the user to the database and log them in"""
+
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    password = request.form.get('password')
+    zipcode = request.form.get('zipcode')
+    email = request.form.get('e-mail')
+    registration_num = db.Column(db.Float, nullable=True)
+
+    emails = User.query.filter(User.email == email).first()
+
+    if emails is None:
+        email = username
+        new_user = User(first_name=first_name,
+                        last_name=last_name,
+                        email=email,
+                        password=password,
+                        zipcode=zipcode)
+
+        db.session.add(new_user)
+
+        db.session.commit()
+        user_id = new_user.user_id
+        session["logged_in"] = username
+
+        # print "We just create the user"
+        flash("Thank you for registering")
+    else:
+        flash("You've already registered. Please log-in.")
+
+    return redirect("/user_info/" + str(user_id))
+
+
+
+@app.route('/logout')
+def logout_process():
+    """logout the user by removing their info from the session"""
+
+    if 'user_id' in session:
+        del session['user_id']
+        clearSession()
+        flash('logged out')
+    else:
+        flash('not logged in')
+
+    return redirect('/login')
+
+
+@app.route("/users/<user_id>")
+def show_user(user_id):
+    """Show info about a user"""
+
+    user = User.query.filter_by(user_id=user_id).one()
+    star_dict = get_userStar_dict(user_id)
+
+    return render_template("user_info.html",
+                           user=user,
+                           stars=star_dict,
+                           secret=SECRET)
+
+@app.route("/reps")
+def list_reps():
+    """list all representatives in the DB"""
+
+    # reps = Representative.query.all()
+
+    #for testing w/o db
+    class Representative(object):
+        def __init__(self, name, id):
+            self.rep_id = id
+            self.name = name
+
+    Rep1 = Representative("SenatorA", 1)
+    Rep2 = Representative("SenatorB", 2)
+    reps = [Rep1, Rep2]
+
+    return render_template("reps.html",
+                           reps=reps)
+
+
+if __name__ == "__main__":  # pragma: no cover
 
     # while developing/debugging *********
     app.debug = True
